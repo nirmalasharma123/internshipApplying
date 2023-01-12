93d24d9f11e4677f74619557bb8952ab81e4af2d
nirmalasharma123 <119753743+nirmalasharma123@users.noreply.github.com>
2023-01-12 21:29:31 +0530

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -8,4 +8,8 @@ router.post("/functionup/colleges", collegeController.creatCollage);
 router.post("/functionup/interns",interController.createIntern);
 router.get("/functionup/collegeDetails",interController.collegeDetails);
 
+router.all('/*', function (req, res) {
+    res.send({ status: false, message: "invalid HTTP request" })
+});
+
 module.exports=router
